05b583bef67cb3839f0e6a78c0fdac856f9a7bd0
luppi <Mikhail.Yakushinale@Gmail.com>
2024-10-21 17:09:24 +0700

    chore: setup import aliases and add ones for ui components

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      "@ui": path.resolve(__dirname, "./src/ui/"),
+    },
+  },
   server: {
     watch: {
       usePolling: true,
