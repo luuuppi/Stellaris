b36a37052c2ba23ebad563e78f585bf4c4126a1b
luppi <Mikhail.Yakushinale@Gmail.com>
2024-10-25 21:05:30 +0700

    feat: implement the logic to delete the ollama session through the dialog

---
+++ src/features/deleteSessionDialog.tsx
@@ -0,0 +1,48 @@
+import { useSessionStore } from "@/store/useSessionsStore";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/ui/alertDialog";
+import Button from "@/ui/button";
+import { forwardRef } from "react";
+
+type DeleteSessionDialogProps = {
+  sessionId: string;
+};
+
+const DeleteSessionDialog = forwardRef<HTMLDivElement, DeleteSessionDialogProps>(
+  ({ sessionId }, ref) => {
+    const deleteSession = useSessionStore((state) => state.deleteSession);
+
+    return (
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <div className="hidden" ref={ref} />
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Delete chat?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action will delete chat <span className="font-bold">Chat name</span>
+          </AlertDialogDescription>
+          <div className="flex justify-end gap-5">
+            <AlertDialogCancel>
+              <Button variant="tertiary">Cancel</Button>
+            </AlertDialogCancel>
+            <AlertDialogAction>
+              <Button variant="danger" onClick={() => deleteSession(sessionId)}>
+                Delete
+              </Button>
+            </AlertDialogAction>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  },
+);
+
+export default DeleteSessionDialog;
