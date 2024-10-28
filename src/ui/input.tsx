dc3dcdd6ddbd4e2a8a02c79311aac84d59ed8912
luppi <Mikhail.Yakushinale@Gmail.com>
2024-10-28 17:01:12 +0700

    feat: add custom className prop to Input component
    - Enhanced `Input` component to accept an optional `className` prop.
    - Utilized `cn` utility to merge default styling with any additional classes provided.
    - Allows external styling customization while retaining base input styles.

--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -1,10 +1,14 @@
+import cn from "@/utils/cn";
 import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from "react";
 
 const Input = forwardRef<ElementRef<"input">, ComponentPropsWithoutRef<"input">>(
-  ({ ...props }, ref) => {
+  ({ className, ...props }, ref) => {
     return (
       <input
-        className="rounded-xl border border-night-600 bg-night-800 p-2 focus:outline-none"
+        className={cn(
+          "rounded-xl border border-night-600 bg-night-800 p-2 focus:outline-none",
+          className,
+        )}
         ref={ref}
         {...props}
       />
