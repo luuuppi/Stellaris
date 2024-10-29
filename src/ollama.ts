281d2038998cfa044854a399a16892b6b5a5d4fb
luppi <Mikhail.Yakushinale@Gmail.com>
2024-10-29 13:04:06 +0700

    feat: refuse ollama-js in favour to ollama rest api
    Ollama REST API turned out to be more convenient to use.

--- src/ollama.ts
+++
@@ -1,8 +0,0 @@
-import { Ollama } from "ollama/browser";
-import { useSettingsStore } from "./store/useSettingsStore";
-
-const ollamaServer = useSettingsStore((state) => state.ollamaServer);
-
-const ollama = new Ollama({ host: ollamaServer });
-
-export default ollama;
