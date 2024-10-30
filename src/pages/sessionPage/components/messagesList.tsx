37bfaf7d47f3ddfc7a60442cb9a688d4e382bfb0
luppi <Mikhail.Yakushinale@Gmail.com>
2024-10-30 14:27:44 +0700

    feat: auto-scroll MessagesList to bottom on new messages
    - Ensured a smooth user experience by always displaying the latest messages.

--- src/pages/sessionPage/components/messagesList.tsx
+++ src/pages/sessionPage/components/messagesList.tsx
@@ -1,14 +1,22 @@
 import { type Message } from "@/store/useSessionsStore";
 import MessageComponent from "@/ui/messageComponent";
-import { type FC } from "react";
+import { useEffect, useRef, type FC } from "react";
 
 type MessagesListProps = {
   messages: Message[];
 };
 
 const MessagesList: FC<MessagesListProps> = ({ messages }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <div className="flex flex-1 flex-col gap-5 overflow-y-scroll px-20 py-8">
+    <div className="flex flex-1 flex-col gap-5 overflow-y-scroll px-20 py-8" ref={ref}>
       {messages.map((message, index) => (
         <MessageComponent key={index} role={message.role === "user" ? "user" : "assistant"}>
           {message.content}
