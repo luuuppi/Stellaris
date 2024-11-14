72a4963d8df4e5af6f03ff5db7077ef4f63024ef
luppi <Mikhail.Yakushinale@Gmail.com>
2024-11-14 21:30:59 +0700

    feat: add displaying of a real-time Ollama completion in messagesList
    - Integrated `completionStore` into `MessagesList` to track in real-time
      completion progress and displaying completion stream.
    - The list auto-scroll to the bottom has been completely rewritten to
      provide better UX.

--- src/pages/sessionPage/components/messagesList.tsx
+++ src/pages/sessionPage/components/messagesList.tsx
@@ -1,28 +1,32 @@
 import { type Message } from "@/store/useSessionsStore";
 import MessageComponent from "@/ui/messageComponent";
-import { useEffect, useRef, type FC } from "react";
+import { type FC } from "react";
+import { StickToBottom } from "use-stick-to-bottom";
+import { useSnapshot } from "valtio";
+import completionStore from "../model/store/completionStore";
 
 type MessagesListProps = {
   messages: Message[];
 };
 
 const MessagesList: FC<MessagesListProps> = ({ messages }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.scrollTop = ref.current.scrollHeight;
-    }
-  }, [messages]);
+  const completionSnap = useSnapshot(completionStore);
 
   return (
-    <div className="flex flex-1 flex-col gap-5 overflow-y-scroll px-20 py-8" ref={ref}>
-      {messages.map((message, index) => (
-        <MessageComponent key={index} role={message.role === "user" ? "user" : "assistant"}>
-          {message.content}
-        </MessageComponent>
-      ))}
-    </div>
+    <StickToBottom className="flex flex-1 overflow-auto" resize="smooth" initial="smooth">
+      <StickToBottom.Content className="flex flex-col gap-5 px-20 py-8">
+        {messages.map((message, index) => (
+          <MessageComponent key={index} role={message.role === "user" ? "user" : "assistant"}>
+            {message.content}
+          </MessageComponent>
+        ))}
+        {completionSnap.isCompletionInProgress && (
+          <MessageComponent role="assistant">
+            {completionSnap.completion === "" ? "Loading model..." : completionSnap.completion}
+          </MessageComponent>
+        )}
+      </StickToBottom.Content>
+    </StickToBottom>
   );
 };
 
