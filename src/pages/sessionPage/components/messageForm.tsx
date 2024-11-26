ee44ce7c7407b34616136367e2d3c5c04b2e05a3
luppi <Mikhail.Yakushinale@Gmail.com>
2024-11-26 18:34:09 +0700

    feat: enhance MessageFrom with textarea input
    - Replaced input field with a textarea in `MessageFrom` to support
      multi-line prompts.
    - Updated UI of `MessageFrom`.
    
    Related changes:
    - Added `field-sizing-content` utility class for better content-based
      resizing of textarea.

--- src/pages/sessionPage/components/messageForm.tsx
+++ src/pages/sessionPage/components/messageForm.tsx
@@ -1,10 +1,17 @@
 import { useSessionStore } from "@/store/useSessionsStore";
 import Button from "@/ui/button";
-import Input from "@/ui/input";
 import { SendHorizontal } from "lucide-react";
-import { type ChangeEvent, type FC, type FormEvent, useCallback, useState } from "react";
-import useOllamaChat from "../model/useOllamaChat";
+import {
+  type ChangeEvent,
+  type FC,
+  type FormEvent,
+  KeyboardEvent,
+  useCallback,
+  useRef,
+  useState,
+} from "react";
 import completionStore from "../model/store/completionStore";
+import useOllamaChat from "../model/useOllamaChat";
 
 type MessageFormProps = {
   id: string;
@@ -12,6 +19,7 @@ type MessageFormProps = {
 
 const MessageForm: FC<MessageFormProps> = ({ id }) => {
   const [value, setValue] = useState<string>("");
+  const formRef = useRef<HTMLFormElement>(null);
   const setMessage = useSessionStore((state) => state.setMessage);
   const ollamaChat = useOllamaChat({
     id,
@@ -20,9 +28,19 @@ const MessageForm: FC<MessageFormProps> = ({ id }) => {
     },
   });
 
-  const changeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  const changeHandler = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value);
   }, []);
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent<HTMLTextAreaElement>) => {
+      if (e.key === "Enter" && !e.shiftKey && formRef.current) {
+        e.preventDefault();
+        formRef.current.requestSubmit();
+      }
+    },
+    [formRef],
+  );
 
   const submitHandler = (e: FormEvent) => {
     e.preventDefault();
@@ -32,19 +50,32 @@ const MessageForm: FC<MessageFormProps> = ({ id }) => {
   };
 
   return (
-    <form className="mx-20 mb-8 flex gap-2" onSubmit={submitHandler}>
-      <Input
-        className="w-full"
-        id="messageInput"
-        placeholder="Write a message..."
-        value={value}
-        onChange={changeHandler}
-        autoComplete="off"
-        autoFocus
-      />
-      <Button size="icon_md">
-        <SendHorizontal />
-      </Button>
+    <form
+      className="mx-20 mb-8 flex rounded-xl border border-night-600 bg-night-800 contain-inline-size"
+      onSubmit={submitHandler}
+      ref={formRef}
+    >
+      <div className="flex w-full items-center gap-1 p-1 pl-2">
+        <textarea
+          className="field-sizing-content better-scrollbar max-h-[10rem] w-full resize-none bg-night-800 leading-[160%] focus:outline-none"
+          rows={1}
+          id="messageInput"
+          placeholder="Write a message..."
+          value={value}
+          onChange={changeHandler}
+          onKeyDown={handleKeyDown}
+          autoComplete="off"
+          autoFocus
+        />
+        <Button
+          className="h-full"
+          size="icon_sm"
+          type="submit"
+          disabled={value === "" ? true : false}
+        >
+          <SendHorizontal />
+        </Button>
+      </div>
     </form>
   );
 };
