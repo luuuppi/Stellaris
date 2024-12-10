cfcf97065ffca9237d52ac59a3d50a63eb0c80d7
luppi <Mikhail.Yakushinale@Gmail.com>
2024-12-10 17:05:48 +0700

    feat(general settings): integrate useOllamaTags hook

--- src/pages/settingsPage/pages/generalSettings.tsx
+++ src/pages/settingsPage/pages/generalSettings.tsx
@@ -1,50 +1,28 @@
 import { useSessionStore } from "@/store/useSessionsStore";
 import { useSettingsStore } from "@/store/useSettingsStore";
 import Button from "@/ui/button";
-import { ModelResponse } from "ollama/browser";
-import { useEffect, useState, type FC } from "react";
-import getOllamaTags from "../api/getOllamaTags";
+import { useState, type FC } from "react";
+import useOllamaTags from "../api/useOllamaTags";
 import ModelSelect from "../components/modelSelect";
 import ServerInput from "../components/serverInput";
 
 const GeneralSettingsPage: FC = () => {
-  const ollamaServer = useSettingsStore((state) => state.ollamaServer);
   const clearSessions = useSessionStore((state) => state.clearSessions);
   const resetSettings = useSettingsStore((state) => state.resetSettings);
-  const [serverStatus, setServerStatus] = useState<"connected" | "disconnected">("connected");
-  const [models, setModels] = useState<ModelResponse[]>([]);
+  const serverStatus = useSettingsStore((state) => state.serverStatus);
   const [isSelectValid, setIsSelectValid] = useState<boolean>(true);
-
-  const getModels = async () => {
-    try {
-      const res = await getOllamaTags(ollamaServer);
-      setModels(res);
-      setServerStatus("connected");
-    } catch (e) {
-      setServerStatus("disconnected");
-    }
-  };
-
-  useEffect(() => {
-    getModels();
-  }, [ollamaServer]);
-
-  useEffect(() => {
-    if (serverStatus === "connected") return;
-
-    const interval = setInterval(() => {
-      getModels();
-    }, 1500);
-
-    return () => clearInterval(interval);
-  }, [serverStatus, getModels]);
+  const { data, refetch, isFetching } = useOllamaTags();
 
   return (
     <main className="flex w-full flex-col">
       <div className="flex flex-col gap-3">
-        <ServerInput serverStatus={serverStatus} checkConnection={getModels} />
+        <ServerInput
+          serverStatus={serverStatus}
+          checkConnection={refetch}
+          isChecking={isFetching}
+        />
         <ModelSelect
-          models={models}
+          models={data?.models ?? []}
           serverStatus={serverStatus}
           isValid={isSelectValid}
           setIsValid={setIsSelectValid}
