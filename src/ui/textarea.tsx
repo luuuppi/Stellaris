f669266f6ffeb96804c0ede2530a110a3b8354e6
luppi <Mikhail.Yakushinale@Gmail.com>
2024-12-16 15:56:08 +0700

    feat(ui): add textarea component

---
+++ src/ui/textarea.tsx
@@ -0,0 +1,20 @@
+import cn from "@/utils/cn";
+import { forwardRef, type ComponentPropsWithoutRef, type ElementRef } from "react";
+
+const Textarea = forwardRef<ElementRef<"textarea">, ComponentPropsWithoutRef<"textarea">>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        className={cn("w-full rounded-xl border border-night-600 bg-night-800 p-1", className)}
+      >
+        <textarea
+          className="better-scrollbar min-h-[5rem] w-full bg-night-800 p-2 focus:outline-none"
+          {...props}
+          ref={ref}
+        />
+      </div>
+    );
+  },
+);
+
+export default Textarea;
