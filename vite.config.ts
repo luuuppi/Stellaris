c9ec838fd8807c061e3d75cc981a4b195e134d8e
luppi <Mikhail.Yakushinale@Gmail.com>
2025-01-25 01:57:02 +0700

    refactor(architecture): move the ui components to the "shared" layer

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
-      "@ui": path.resolve(__dirname, "./src/ui/"),
+      "@ui": path.resolve(__dirname, "./src/shared/ui/"),
     },
   },
   server: {
