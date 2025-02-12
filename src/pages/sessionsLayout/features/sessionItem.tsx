2d29d3bd96d7fa8297effb279aa8769ffb9654fd
luppi <Mikhail.Yakushinale@Gmail.com>
2025-02-12 23:49:45 +0700

    feat(sidebar): add simple animation
    Created enter and exit animations for session items and groups.

--- src/pages/sessionsLayout/features/sessionItem.tsx
+++ src/pages/sessionsLayout/features/sessionItem.tsx
@@ -1,7 +1,8 @@
 import { useSessionStore, selectSessionName } from "@/store/useSessionsStore";
 import { Link } from "@tanstack/react-router";
 import SessionActionsDropdown from "./sessionActionsDropdown";
-import { memo, type FC } from "react";
+import { motion } from "framer-motion";
+import { forwardRef, memo } from "react";
 
 type SessionItemProps = {
   title: string;
@@ -9,22 +10,32 @@ type SessionItemProps = {
   isSelected: boolean;
 };
 
-const SessionItem: FC<SessionItemProps> = memo(({ title, sessionId, isSelected }) => {
-  const sessionName = useSessionStore(selectSessionName(sessionId));
+const SessionItem = memo(
+  forwardRef<HTMLDivElement, SessionItemProps>(({ title, sessionId, isSelected }, ref) => {
+    const sessionName = useSessionStore(selectSessionName(sessionId));
 
-  return (
-    <Link
-      to={`/sessions/${sessionId}`}
-      params={{ sessionId }}
-      data-selected={isSelected}
-      className="flex w-full cursor-pointer items-center justify-between rounded-xl px-3 py-2 transition-colors duration-200 ease-in-out hover:bg-night-700/50 active:bg-night-700/30 data-[selected=true]:bg-accent-500/30 data-[selected=true]:hover:bg-accent-500/25 data-[selected=true]:active:bg-accent-500/20"
-    >
-      <span className="overflow-hidden text-ellipsis whitespace-nowrap font-medium text-neutral-300">
-        {sessionName || title}
-      </span>
-      <SessionActionsDropdown sessionId={sessionId} sessionName={sessionName ?? title} />
-    </Link>
-  );
-});
+    return (
+      <motion.div
+        layout
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        ref={ref}
+      >
+        <Link
+          to={`/sessions/${sessionId}`}
+          params={{ sessionId }}
+          data-selected={isSelected}
+          className="flex w-full cursor-pointer items-center justify-between rounded-xl px-3 py-2 transition-colors duration-200 ease-in-out hover:bg-night-700/50 active:bg-night-700/30 data-[selected=true]:bg-accent-500/30 data-[selected=true]:hover:bg-accent-500/25 data-[selected=true]:active:bg-accent-500/20"
+        >
+          <span className="overflow-hidden text-ellipsis whitespace-nowrap font-medium text-neutral-300">
+            {sessionName || title}
+          </span>
+          <SessionActionsDropdown sessionId={sessionId} sessionName={sessionName ?? title} />
+        </Link>
+      </motion.div>
+    );
+  }),
+);
 
 export default SessionItem;
--- src/pages/sessionsLayout/widgets/sessionsList.tsx
+++ src/pages/sessionsLayout/widgets/sessionsList.tsx
@@ -3,6 +3,7 @@ import { useShallow } from "zustand/react/shallow";
 import useGroupedSessions from "../utils/useGroupedSessions";
 import { useParams } from "@tanstack/react-router";
 import SessionItem from "../features/sessionItem";
+import { AnimatePresence, motion } from "framer-motion";
 import { type FC } from "react";
 
 const SessionsList: FC = () => {
@@ -11,19 +12,23 @@ const SessionsList: FC = () => {
   const { sessionId: currentId } = useParams({ strict: false });
 
   return (
-    <nav className="better-scrollbar mb-auto flex w-full flex-col gap-6 overflow-auto">
+    <nav className="better-scrollbar flex h-full w-full flex-col gap-6 overflow-auto">
       {groupedSessions.map((gp) => (
         <div key={gp.label}>
-          <span className="mb-3 ml-2 inline-block text-base font-semibold">{gp.label}</span>
+          <motion.span layout className="mb-3 ml-2 inline-block text-base font-semibold">
+            {gp.label}
+          </motion.span>
           <div className="flex flex-col gap-1">
-            {gp.sessions.map((session) => (
-              <SessionItem
-                title={session.messages[0] ? session.messages[0].content : session.id}
-                sessionId={session.id}
-                key={session.id}
-                isSelected={currentId === session.id}
-              />
-            ))}
+            <AnimatePresence initial={false} mode="popLayout">
+              {gp.sessions.map((session) => (
+                <SessionItem
+                  title={session.messages[0] ? session.messages[0].content : session.id}
+                  sessionId={session.id}
+                  key={session.id}
+                  isSelected={currentId === session.id}
+                />
+              ))}
+            </AnimatePresence>
           </div>
         </div>
       ))}
